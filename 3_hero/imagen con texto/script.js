595fc34f8a4c38e6cb3b78a46f223c933dd17b4b
Tommy & Lexi <somos.tl.dev@gmail.com>
2026-05-23 18:31:07 -0300

    Add files via upload

---
+++ 3_hero/imagen con texto/script.js
@@ -0,0 +1,16 @@
+/* 
+    javascript de hamburguesa
+    ##########################################################################################################
+
+*/
+
+const hamburguesa = document.getElementById("hamburguesa");
+const menu = document.getElementById("menu-desplegable");
+
+hamburguesa.addEventListener("click", () => {
+    menu.classList.toggle("activo");
+
+
+hamburguesa.classList.toggle("activo");
+});
+
