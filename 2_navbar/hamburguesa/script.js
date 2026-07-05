8193c145c04228e5b473a570ce73452d2752721c
Tommy & Lexi <somos.tl.dev@gmail.com>
2026-07-05 15:42:23 -0300

    Delete 2_navbar directory

--- 2_navbar/hamburguesa/script.js
+++
@@ -1,9 +0,0 @@
-const hamburguesa = document.getElementById("hamburguesa");
-const menu = document.getElementById("menu-desplegable");
-
-hamburguesa.addEventListener("click", () => {
-    menu.classList.toggle("activo");
-
-
-hamburguesa.classList.toggle("activo");
-});
